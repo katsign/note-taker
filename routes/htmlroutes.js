6768b8c1932f95e29b80d29823bc30427c44917d
Kat Buchanan <katrbuch@gmail.com>
2021-02-23 11:28:11 -0600

    fixing path errors

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,11 +3,11 @@ const path = require('path');
 // Writes paths to HTML pages
 module.exports = (app) => {
     app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/notes.html'));
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '..public/index.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
     // In case of no matching route, default to index
     app.get('*', (req, res) => {
