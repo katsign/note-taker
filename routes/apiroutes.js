35a79731c47bf38bbcda05812ee4db8af84929f7
Kat Buchanan <katrbuch@gmail.com>
2021-02-26 13:30:33 -0600

    working on delete block

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -35,4 +35,26 @@ app.post('/api/notes', (req, res) => {
     res.status(200).send();
 });
 
+// Working on Deletes...
+app.delete('/api/notes/:id', (req, res) => {
+    // Receive a query param w/ ID
+    const deleteNote = req.params.id;
+    // Read all notes in DB
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        // Check chosen ID against all IDs in DB, splice out when found
+        dbData = JSON.parse(data);
+        for(let i = 0; i < dbData.length; i++) {
+            if (dbData[i].id === Number(deleteNote)) {
+                dbData.splice([i], 1);
+            }
+        }
+        stringData = JSON.stringify(dbData);
+        // Rewrite the new set to DB
+        fs.writeFile('./db/db.json', stringData, (err, data) => {
+            if (err) throw err;
+        });
+    });
+res.status(204).send();
+});
 };
